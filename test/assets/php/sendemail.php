522bfb57d8a742717f0c194f88d1317c511f004a
gusy <rubensancor@opendeusto.es>
2017-12-04 00:20:19 +0100

    php32

--- test/assets/php/sendemail.php
+++ test/assets/php/sendemail.php
@@ -1,9 +1,9 @@
 <?php
 
-	$name = @trim(stripslashes($_POST['name'])); 
-	$email = @trim(stripslashes($_POST['email']));  
-	$subject = @trim(stripslashes($_POST['subject']));  
-	$message = @trim(stripslashes($_POST['message'])); 
+	$name = $_POST['name']); 
+	$email = $_POST['email']);  
+	$subject = $_POST['subject']);  
+	$message = $_POST['message']); 
 
 	$email_from = $email;
 	$email_to = 'rubensancor@gmail.com'; //replace with your email
