a0329adf6f8bad04a858b42622eb725cda9a88fc
Ruben Sanchez <rubensancor@opendeusto.es>
2017-12-06 23:24:42 +0100

    Final version

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -1,22 +1,23 @@
 <?php
 
-	$name = @trim(stripslashes($_POST['name'])); 
-	$email = @trim(stripslashes($_POST['email']));  
-	$subject = @trim(stripslashes($_POST['subject']));  
-	$message = @trim(stripslashes($_POST['message'])); 
+	$name = $_POST['name']); 
+	$email = $_POST['email']);  
+	$subject = $_POST['subject']);  
+	$message = $_POST['message']); 
 
 	$email_from = $email;
-	$email_to = 'yourmail@gmail.com'; //replace with your email
+	$email_to = 'rubensancor@gmail.com'; //replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
-	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
+	$success = @mail("rubensancor@gmail.com", "asdf", "asdf");
 	
 ?>
 
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html>
 <head>
 	<script>alert("Thank you for contact us. As early as possible  we will contact you.");</script>
-	<meta HTTP-EQUIV="REFRESH" content="0; url=http://trendytheme.net/demo/iamx/v/"> 
-</head>
+	<meta charset="UTF-8" HTTP-EQUIV="REFRESH" content="0; url=https://rubensancor.github.io/test/index.html"> 
+</head> 
+</html>
